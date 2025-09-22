968788c9d858509241ad034efe635fb1a574e355
RedVelvetCake11 <membermadde.insiddev25120@hotmail.com>
2025-09-22 16:27:59 +0530

    .

--- proxmox/ve/nextstep.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo Adding NeXTSTEP repo to APT repositories...
-touch /etc/apt/sources.list
-echo "deb https://aphid-promoted-finch.ngrok-free.app/repo/proxmox/nextstep bookworm" >> /etc/apt/sources.list
-echo Updating APT repositories...
-apt update
-echo Done.
